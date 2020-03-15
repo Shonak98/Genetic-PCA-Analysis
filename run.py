24b1bd2d58ac5a2acf5ee1c003a1590dcd4a94f6
Shonak98 <33361172+Shonak98@users.noreply.github.com>
2020-03-15 13:58:02 -0700

    Plot and Params
    Added Plot titling and parameterized vcf arguments

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from etl import *
 def load(params):
     links = params['chromosome_links']
     outpath = params['outpath']
-    
+
     for link in links:
         file_name = link[-link[::-1].find('/'):]
         data_pull(link, file_name, outpath)
@@ -15,7 +15,7 @@ def load(params):
 
 
 def project(params):
-    
+
     ######## create reference files
     create_references(params["references"], params["new_ref_location"])
 
@@ -23,35 +23,35 @@ def project(params):
     fasta = params["new_ref_location"] + '/' + get_fasta_location(params["new_ref_location"])
     fastq_to_bam(params['test_fastq'], fasta, params['conversion_dir'], result_name = params['bam_name'])
     bam_to_vcf(params['bam_name'], fasta, params['conversion_dir'], output_name = params['vcf_name'])
-    
-    ######## vcf analysis 
+
+    ######## vcf analysis
     chrom_lst = get_chrom_list(params['chromosomes'])
     directory = params['directory']
     extension = params['extension']
     vcf_dir = params['vcf_dir']
-    
+
     vcf_lst = [f'{directory}{chrom}{extension}' for chrom in chrom_lst]
-    
+
     for ind, vcf in enumerate(vcf_lst):
-        filtering_vcf(vcf, chrom_lst[ind], vcf_dir, params['maf'], 
-                   params['geno'], params['mind'], 'vcf', keep_files = False)
-        
+        filtering_vcf(vcf, chrom_lst[ind], vcf_dir, params['maf'],
+                   params['geno'], params['mind'], param['fil_output'],
+                   keep_files = params['keep_filtered'])
+
     zipped_lst = [f"{chrom}.vcf.gz" for chrom in chrom_lst]
     vcf_concat(zipped_lst, params['merged_vcf'], vcf_dir)
-    
-    pca(params['merged_vcf'], 'vcf', params['eigen_file'], vcf_dir)
-    
+
+    pca(params['merged_vcf'], param['fil_output'], params['eigen_file'], vcf_dir)
+
     eigen_file = vcf_dir + '/' + params['eigen_file'] + '.eigenvec'
-    plot_clusters(eigen_file, picture_name = params['picture_name'])
-    
-        
+    plot_clusters(eigen_file, chrom_lst, picture_name = params['picture_name'])
+
+
 if __name__ ==  '__main__':
     arguments = sys.argv
     params = json.load(open("config/params.json"))
-    
+
     if 'load' in arguments:
         load(params)
-    
+
     if 'test-project' in arguments:
         project(params)
-    
--- src/etl.py
+++ src/etl.py
@@ -16,14 +16,14 @@ import time
 
 def get_chrom_list(chr_lst):
     chromes = []
-    
+
     for ind, curr in enumerate(chr_lst):
         if curr == '-':
             chromes += list(range(chr_lst[ind-1]+1, chr_lst[ind+1]))
         else:
             chromes.append(curr)
     arr = np.array(chromes)
-    
+
     return np.unique(arr[(arr>0) & (arr<23)])
 
 
@@ -35,11 +35,11 @@ def get_files_in_directory(directory):
     locations_file = "locations.txt"
     if os.path.isdir(directory):
         os.system(f"cd {directory}; dir>{locations_file}")
-    
+
         with open(directory + f'/{locations_file}') as fh:
             for i in fh:
                 files += i.split()
-        
+
         os.system(f"rm -rf {directory}/{locations_file}")
     return files
 
@@ -68,23 +68,23 @@ def create_references(location, references):
         location += '/'
     if os.path.isdir(references) == False:
         os.system(f'mkdir {references}')
-    
+
     fasta = get_fasta_location(references)
-    
-    if (fasta == '') and (validate_dict_file(references) == False): 
+
+    if (fasta == '') and (validate_dict_file(references) == False):
         os.system(f"cp -r {location}* {references}")
-    
-    
+
+
     fasta = get_fasta_location(references)
     os.system(f"samtools faidx {references}/{fasta}")
-    
+
     if validate_dict_file(references) == False:
         dict_file = fasta[:-6] + ".dict"
         os.system(f"gatk CreateSequenceDictionary --REFERENCE '{references}/{fasta}' \
                    --OUTPUT '{references}/{dict_file}'")
-    
 
-    
+
+
 ################## data pulling
 
 def data_pull(chromosomes_paths, output_name, output_dir = None):
@@ -94,7 +94,7 @@ def data_pull(chromosomes_paths, output_name, output_dir = None):
     into. The files are downloaded to the same format as they were in the
     server and then unzipped to be usable for analysis.
     """
-    
+
     if output_dir != None:
         if os.path.exists(output_dir) == False:
             os.mkdir(output_dir)
@@ -118,47 +118,47 @@ def unzip(file):
             shutil.copyfileobj(f_in, f_out)
             time.sleep(0.01)
     return
-    
-    
-    
+
+
+
 ################## data conversion functions
 
 def fastq_to_bam(fastq, fasta, directory, read_group = "default", result_name = None, keep_sam = True):
     if os.path.isdir(directory) == False:
         os.system(f"mkdir {directory}")
-    
+
     no_ext = fastq[-fastq[::-1].find('/'):-3]
     if result_name != None:
         no_ext = str(result_name)
-        
+
     if no_ext[-4:] == ".bam":
         no_ext = no_ext[:-4]
-    
+
     os.system("chmod 700 src/fq_conversion.sh")
     if read_group == "default":
         os.system(f"./src/fq_conversion.sh {fastq} {fasta} {directory}/{no_ext}")
     else:
         os.system(f"./src/fq_conversion.sh {fastq} {fasta} {directory}/{no_ext} {read_group}")
-        
+
     if not keep_sam:
         os.system(f"rm -rf {no_ext}.sam")
-              
+
 
 def bam_to_vcf(bam, fasta, directory, output_name = None):
     if os.path.isdir(directory) == False:
         os.system(f"mkdir {directory}")
-    
+
     if '/' in bam:
         no_ext = bam[-bam[::-1].find('/'):-4]
     else:
         no_ext = bam[:-4]
-        
+
     if output_name != None:
         no_ext = str(output_name)
-    
+
     os.system("chmod 700 src/bam_conversion.sh")
     os.system(f"./src/bam_conversion.sh {directory}/{bam} {fasta} {directory}/{no_ext}")
-            
+
 
 
 ################## data manipulations
@@ -166,33 +166,33 @@ def bam_to_vcf(bam, fasta, directory, output_name = None):
 def filtering_vcf(vcf, output_name, directory, maf, geno, mind, output_type = '', keep_files = True):
     if os.path.isdir(directory) == False:
         os.system(f"mkdir {directory}")
-    
+
     os.system("chmod 700 src/filtering.sh")
-    
+
     if output_type != '':
         os.system(f"./src/filtering.sh {vcf} {directory} {output_name} {maf} {geno} {mind} {output_type}")
     else:
         os.system(f"./src/filtering.sh {vcf} {directory} {output_name} {maf} {geno} {mind}")
-    
-    if "vcf" in output_type: 
+
+    if "vcf" in output_type:
         os.system(f"bgzip -f {directory}/{output_name}.vcf > {directory}/{output_name}.vcf.gz")
-    
+
     if keep_files == False:
         os.system(f'rm -rf {directory}/{output_name}.bed')
         os.system(f'rm -rf {directory}/{output_name}.bim')
         os.system(f'rm -rf {directory}/{output_name}.bam')
         os.system(f'rm -rf {directory}/{output_name}.nosex')
-    
+
 
 def vcf_concat(vcf_lst, output, directory):
     string_lst = "bcftools concat "
     for vcf in vcf_lst:
         string_lst += f"{directory}/{vcf} "
-    
-    string_lst += f"-o {directory}/{output}" 
+
+    string_lst += f"-o {directory}/{output}"
     os.system(string_lst)
-    
-    
+
+
 def pca(file, file_type, output, directory, pca_num = 2):
     if file_type == 'vcf':
         os.system(f"plink2 --vcf {directory}/{file} --pca {pca_num} --out {directory}/{output}")
@@ -202,10 +202,11 @@ def pca(file, file_type, output, directory, pca_num = 2):
 
 ################## analysis functions
 
-def plot_clusters(eig_file, picture_name = None, codes_file = "data/igsr_samples.tsv", with_colors = True):
+def plot_clusters(eig_file, chrom_lst, picture_name = None, codes_file = "data/igsr_samples.tsv",
+                  with_colors = True):
     """
     Plots clusters from filtered genetic data
-    
+
     @param eig_file : 4 column eigenvec file created from plink2 pca
     @param picture_name: (optional) file name that cluster plot will be saved to
     @param pop_codes : (optional) igsr_samples.tsv default (comes from 1000 Genomes page)
@@ -213,14 +214,18 @@ def plot_clusters(eig_file, picture_name = None, codes_file = "data/igsr_samples
     """
     eigen_df = pd.read_csv(eig_file, sep = ' ', names = ['Sample name', 'Population', 'x', 'y'])
     pop_codes = pd.read_csv(codes_file, sep = '\t')[['Sample name', 'Superpopulation code']]
-    
+
     super_code = eigen_df.join(pop_codes.set_index('Sample name'), on = 'Sample name')
-    
+
     if with_colors:
         sns.scatterplot(x = super_code.x, y = super_code.y, hue = super_code['Superpopulation code'])
         plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     else:
         sns.scatterplot(x = super_code.x, y = super_code.y, edgecolor = None, s= 5)
-    
+
+    plt.title(f"PCA Analysis on Chromosomes {', '.join(map(str,chrom_lst))}")
+    plt.xlabel("PCA Component 1")
+    plt.ylabel("PCA Component 2")
+
     if picture_name != None:
         plt.savefig(picture_name, bbox_inches='tight')   
